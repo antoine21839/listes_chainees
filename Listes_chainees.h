8117fa80f683af3edffb390a432bc7d1754b5b6b
Antoine B <antoine21839@gmail.com>
2022-05-22 18:44:54 +0200

    Update Listes_chainees.h

--- Listes_chainees.h
+++ Listes_chainees.h
@@ -7,10 +7,10 @@ typedef struct cellule{
 }Cellule, *Liste;
 
 Cellule * alloue_cellule(int valeur);
-int ajoute_cellule_a_la_fin(Liste *lst, int valeur);
-int ajoute_cellule_au_debut(Liste *lst, int valeur);
+int ajoute_cellule_fin(Liste *lst, int valeur);
+int ajoute_cellule_debut(Liste *lst, int valeur);
+void supprime_cellule_debut(Liste *lst);
 void supprime_cellule_fin(Liste *lst);
 Cellule * recherche(Liste lst, int valeur);
 void affiche_liste(Liste lst);
-void detruire_cell(Liste *lst, Cellule *cell);
-int ajout_debut(Liste *lst, int val);
+void parcours_liste(Liste lst, void (*appliquer)(Cellule *));
